07e478f4b34dfce0376cb474a2c42c43f666f284
Adrian Stabiszewski <as@grundid.de>
2015-12-31 10:23:17 +0100

    dot input added

--- app/src/main/java/de/grundid/drinker/EditDrinkActivity.java
+++ app/src/main/java/de/grundid/drinker/EditDrinkActivity.java
@@ -22,7 +22,6 @@ import de.grundid.drinker.utils.DrinkModelHelper;
 import de.grundid.drinker.utils.PreferencesUtils;
 
 import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +47,7 @@ public class EditDrinkActivity extends AppCompatActivity {
 		protected char[] getAcceptedChars() {
 			return new char[] {
 					'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',',
-					new DecimalFormatSymbols().getDecimalSeparator() };
+					'.' };
 		}
 
 		public int getInputType() {
